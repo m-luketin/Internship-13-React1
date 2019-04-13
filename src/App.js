2f07b4dcadafb273e8263863506bae818595217a
m-luketin <ml46019@oss.unist.hr>
2019-04-13 11:16:22 +0200

    Fixed redundancies and errors

--- src/App.js
+++ src/App.js
@@ -8,68 +8,42 @@ class App extends Component {
 
         this.state = {
             color: 'black',
-            active: [true, true, true],
-            bold: [false, false, false, false, false, false]
+            active: [true, true, true]
         }
     }
-
+    
+    changeColorHandler = (color) => {    
+        this.setState({color});
+    }
+    
+    toggle = (id) => {
+        this.setState(previousState => {
+            let tmpStateActive = previousState.active;
+            tmpStateActive[id] = !tmpStateActive[id];
+            return {
+                active: tmpStateActive
+            };
+        })
+    }
+    
     render(){
         return (
             <>
                 <h2>Color</h2>
-                <span onClick={() => this.ChangeColorHandler("red", 0)} style={ this.state.bold[0] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Red</span><br />
-                <span onClick={() => this.ChangeColorHandler("blue", 1)} style={ this.state.bold[1] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Blue</span><br />
-                <span onClick={() => this.ChangeColorHandler("green", 2)} style={ this.state.bold[2] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Green</span><br />
+                <span onClick={() => this.changeColorHandler("red", 0)} style={ this.state.color === "red" ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Red</span><br />
+                <span onClick={() => this.changeColorHandler("blue", 1)} style={ this.state.color === "blue" ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Blue</span><br />
+                <span onClick={() => this.changeColorHandler("green", 2)} style={ this.state.color === "green" ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>Green</span><br />
                 <h2>Number</h2>
-                <span onClick={() => this.Toggle(0, 0)} style={ this.state.bold[3] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>1</span><br />
-                <span onClick={() => this.Toggle(1, 1)} style={ this.state.bold[4] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>2</span><br />
-                <span onClick={() => this.Toggle(2, 2)} style={ this.state.bold[5] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>3</span><br />
+                <span onClick={() => this.toggle(0, 0)} style={ this.state.active[0] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>1</span><br />
+                <span onClick={() => this.toggle(1, 1)} style={ this.state.active[1] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>2</span><br />
+                <span onClick={() => this.toggle(2, 2)} style={ this.state.active[2] ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}>3</span><br />
                 <h2>ColoredText</h2>
-                <ColoredText value={1} color={this.state.color} active={this.state.active[0]} />
-                <ColoredText value={2} color={this.state.color} active={this.state.active[1]} />
-                <ColoredText value={3} color={this.state.color} active={this.state.active[2]} />
+                <ColoredText label={1} color={this.state.active[0] ? this.state.color : "black"}/>
+                <ColoredText label={2} color={this.state.active[1] ? this.state.color : "black"}/>
+                <ColoredText label={3} color={this.state.active[2] ? this.state.color : "black"}/>
             </>
         )
     }
-    
-    ChangeColorHandler = (color, buttonId) => {    
-        this.setState({color: color});
-        this.BoldColorButton(buttonId);
-    }
-    
-    Toggle = (id, buttonId) => {
-        this.setState(previousState => {
-            previousState.active[id] = !previousState.active[id];
-            return {
-                active: previousState.active
-            };
-        })
-        this.BoldIndex(buttonId);
-    }
-    
-    
-    BoldColorButton = (buttonId) => {
-        this.setState(previousState => {
-            for(let i = 0; i < 3; i++){
-                if(i === buttonId)
-                    previousState.bold[i] = !previousState.bold[buttonId];
-                else
-                    previousState.bold[i] = false;
-            }
-            return {
-                button: previousState.button
-            };
-        })
-    }
-
-    BoldIndex = (buttonId) => {
-        this.setState(previousState => {
-            previousState.bold[buttonId + 3] = !previousState.bold[buttonId + 3];
-            return {
-                button: previousState.button
-            };
-        })
-    }
 } 
 
-export default App;
+export default App; 
--- src/components/ColoredText.js
+++ src/components/ColoredText.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 export default (props) => {
-    if(props.active)
-        return <div className="colored-text" style={{color: props.color}} >{props.value}</div>
-
-    return <div className="colored-text">{props.value}</div>;
+    return <div className="colored-text" style={{color: props.color}} >{props.label}</div>
 };
     
